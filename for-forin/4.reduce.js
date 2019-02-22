1d88f2694d4738007c336457213f39e0b34f5912
lucas2502 <lucas2502@live.com>
2019-02-22 10:56:59 -0300

    Usando e criando um array.Reduce

--- for-forin/4.reduce.js
+++ for-forin/4.reduce.js
@@ -1,5 +1,14 @@
 const { obterPessoas } = require('./service')
 
+// reduce feito a mão ;)
+Array.prototype.meuReduce = function (callback, valorInicial){
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+    for( let index = 0; index <= this.length -1; index ++ ){
+        valorFinal = callback(valorFinal, this[index], this)
+    }
+    return valorFinal
+}
+
 async function main() {
     try {
         const{ results } = await obterPessoas('a')
@@ -11,6 +20,7 @@ async function main() {
         })
 
         console.log('total', total)
+
     } catch (error) {
         console.error('DEU RUIM', error)
     }
