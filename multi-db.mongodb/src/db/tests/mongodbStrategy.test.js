4bb6a01149d26edd49ad990f309958dde849a45a
lucas2502 <lucas2502@live.com>
2019-04-17 17:13:41 -0300

    Create Heroes

--- multi-db.mongodb/src/db/tests/mongodbStrategy.test.js
+++ multi-db.mongodb/src/db/tests/mongodbStrategy.test.js
@@ -2,8 +2,11 @@ const assert = require('assert')
 const MongoDb = require('./../strategies/mongodb')
 const Context = require('./../strategies/base/contextStrategy')
 
+const MOCK_HEROIS_CADASTRAR = {
+    nome: 'Mulher maravilha',
+    poder: 'Laço'
+}
 const context = new Context(new MongoDb())
-
 describe('MongoDB suite de testes', function () {
     this.beforeAll(async () => {
         await context.connect()
@@ -13,5 +16,10 @@ describe('MongoDB suite de testes', function () {
         const expected = "conectado"
 
         assert.deepEqual(result, expected)
+    })
+
+    it('cadastrar', async () => {
+        const { nome, poder} = await context.create(MOCK_HEROIS_CADASTRAR)
+        assert.deepEqual({ nome,poder}, MOCK_HEROIS_CADASTRAR )
     })
 })
